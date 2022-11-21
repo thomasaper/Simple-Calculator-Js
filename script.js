7a6db0d8644f04eec717c9a5db98d58f02d03163
Thomas Aper <thomasaper@gmx.de>
2022-11-21 14:05:53 +0100

    added variable elements

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const references to dom elements
+// constant references to dom elements
 const display1El = document.querySelector('.display-1');
 const display2El = document.querySelector('.display-2');
 const tempResultEl = document.querySelector('.temp-result');
@@ -7,3 +7,10 @@ const operationsEl = document.querySelectorAll('.operation');
 const equalEl = document.querySelector('.equal');
 const clearEl = document.querySelector('.all-clear');
 const clearLastEl = document.querySelector('.last-entity-clear');
+
+// variable elements
+let dis1Num = '';
+let dis2Num = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;

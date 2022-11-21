ea53939496c3433ef6b25e198c19a44eb6da4087
Thomas Aper <thomasaper@gmx.de>
2022-11-21 14:00:17 +0100

    added const references to dom elements

---
+++ script.js
@@ -0,0 +1,9 @@
+// const references to dom elements
+const display1El = document.querySelector('.display-1');
+const display2El = document.querySelector('.display-2');
+const tempResultEl = document.querySelector('.temp-result');
+const numbersEl = document.querySelectorAll('.number');
+const operationsEl = document.querySelectorAll('.operation');
+const equalEl = document.querySelector('.equal');
+const clearEl = document.querySelector('.all-clear');
+const clearLastEl = document.querySelector('.last-entity-clear');

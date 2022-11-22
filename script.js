a8e6618f3c05d915bbd6bf62bdc1350d260c29be
Thomas Aper <thomasaper@gmx.de>
2022-11-22 08:12:32 +0100

    added functionality for numbers and dot

--- script.js
+++ script.js
@@ -14,3 +14,22 @@ let dis2Num = '';
 let result = null;
 let lastOperation = '';
 let haveDot = false;
+
+// event listeners
+numbersEl.forEach( number => {
+    number.addEventListener('click', (e) => {
+        // if we click dot and we have no dot previously go further
+        if(e.target.innerText === '.' && !haveDot) {
+            haveDot = true
+        // if we click dot and we have allready a dot just return
+        } else if (e.target.innerText === '.' && haveDot) {
+            return;
+        }
+        // change numbers in display
+        // if we click number add number to variable
+        dis2Num += e.target.innerText
+        // actually show the number in display2El
+        display2El.innerText = dis2Num;
+
+    })
+})
